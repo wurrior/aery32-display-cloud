b306b318b4ab651a3682a2a05a5559ddfc0b8c57
Markus Vuorio <markus.vuorio@stemlux.fi>
2013-04-21 15:49:56 +0300

    Minor fixes to examples
    Changed comments and removed unnecessary variables from udraw.

--- examples/clouds/display/calibrate_touch.cpp
+++ examples/clouds/display/calibrate_touch.cpp
@@ -1,22 +1,27 @@
-#include <aery32/all.h>
-#include "board.h"
+/**
+ * \file calibrate_touch.cpp
+ * \brief Aery32 Display Cloud Calibration Sequence. 
+ * Run this example when using your Display Cloud the first time
+ * or when your display needs calibration.
+ *
+ * Tap and hold on the marker for atleast one second. Release and the
+ * next one appears, if not recycle the power and start again.
+ */
 
-#include "clouds/display/display.h"
+#include <aery32/all.h> // aery32 headers
+#include "board.h" // platform settings
+#include "clouds/display/display.h" // display cloud headers
 
 int main(void)
 {
-	board::init();
-
-	/*
-	 * Aery32 Display Cloud Calibration Sequence. Run this example when
-	 * using your Display Cloud the first time or when your display needs
-	 * calibrating.
-	 *
-	 * Tap and hold on the marker for atleast one second. Release and the
-	 * next one appears, if not recycle the power and start again.
-	 */
-	display::lcd_init();        // Initialize display
-	display::touch_init();      // Initialize touch controller
-	display::touch_calibrate(); // Run the calibration sequence	
-	display::test_image();      // Show that the calibration has ended
+	// Initialize aery32:
+	board::init();				
+	// Initialize lcd panel:
+	display::lcd_init();
+	// Initialize touch controller:
+	display::touch_init();
+	// Run the calibration sequence:
+	display::touch_calibrate();
+	// Show test screen to mark the end of calibration:
+	display::test_image();      
 }
--- examples/clouds/display/udraw.cpp
+++ examples/clouds/display/udraw.cpp
@@ -1,37 +1,44 @@
-#include <aery32/all.h>
-#include "board.h"
+/**
+ *	\file udraw.cpp
+ *	\brief uDraw is a simple example on how to accept and handle touch reports.
+ */
 
-#include "clouds/display/display.h"
+#include <aery32/all.h> // aery32 headers
+#include "board.h" // platform settings
+#include "clouds/display/display.h" // display headers
 
 int main(void)
 {
-	/* Declare some variables */
-	display::touch_t touch;
-	int last_x = 0, last_y = 0;
-	uint8_t last_state = 0;
+	display::touch_t current, last; // touch events
 
-	board::init();
+	board::init(); // initializes platform
 
-	/* µDraw is a simple example on how to accept and handle touch reports */
-	display::lcd_init();
-	display::touch_init();
-	display::touch_enable();
+	display::lcd_init(); // initializes lcd panel
+	display::touch_init(); // initializes touch controller
+	display::touch_enable(); // enables touch controller
 
-	/* Empty the display (fill the display from x,y to x,y, color FFFF "white") */
+	/* Empty the display by filling a 240x400px rectangle with 
+	white(color code: 0xFFFF): */
 	display::fill_rectangle(0,0,240,400,0xFFFF);
 
 	for(;;) {
+		// Wait for touch reports:
+		display::touch_wait_for_data();
+		// Get the received data:
+		current = display::touch_get_data();
 
-		display::touch_wait_for_data();          //Wait for touch reports
-		touch = display::touch_get_data();       //Get the received data
-			if(last_state && touch.penstate) //Check if pen was lifted
-				{
-					display::draw_line( touch.x, touch.y, last_x, last_y, 0 ); //if not draw a line between two reports (from x,y to x,y color 0 "black")
-				}
-
-			last_x = touch.x; //set the current report into variables
-			last_y = touch.y;
-			last_state = touch.penstate;
+		// Compare pen states:
+		if(current.penstate && last.penstate)
+		{
+			/* Both are 1, so pen is still down. 
+			Draw a black line (color code: 0) 
+			between last and current pen postion: */
+			display::draw_line( current.x, current.y, last.x, last.y, 0 );
+		}
+		
+		// store current event:	
+		last = current;
+		
 	}
 
 	return 0;
